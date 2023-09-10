a44a1951f50c3dc47a0df224968eff12ca0e7460
Daniel Yang <danielyang@Daniels-MacBook-Pro.local>
2023-09-10 20:21:58 +0800

    login logout

--- app/components/Connect.js
+++ app/components/Connect.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import MyAlgoConnect from '@randlabs/myalgo-connect';
 import { doc, query, collection, where, getDocs } from 'firebase/firestore';
 import { storage, database } from '../firebaseConfig';
@@ -10,6 +10,9 @@ const Connect = () => {
   const router = useRouter();
   const walletAddress = useSelector((state) => state.wallet.walletAddress);
   const dispatch = useDispatch();
+
+  const [showModal, setShowModal] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   // Load wallet address from local storage on component mount
   useEffect(() => {
@@ -58,15 +61,40 @@ const Connect = () => {
     }
   };
 
+  const handleLogout = () => {
+    // Clear the wallet address from Redux and local storage
+    dispatch(setWalletAddress(''));
+    localStorage.removeItem('walletAddress');
+    // Close the modal
+    setShowModal(false);
+  };
+
   return (
     <div className="connect-button">
       {!walletAddress ? (
         <button onClick={handleConnect}>Connect Wallet</button>
       ) : (
         <div>
-          <p>{walletAddress.slice(0, 4)}...{walletAddress.slice(-4)}</p>
+          <p
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            {isHovered ? <button onClick={() => setShowModal(true)}>Logout</button> : `${walletAddress.slice(0, 4)}...${walletAddress.slice(-4)}`}
+          </p>
         </div>
       )}
+
+      {/* Modal */}
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <p>Are you sure you want to logout?</p>
+            <button className="logout-button" onClick={handleLogout}>Yes</button>
+            <button className="logout-button" onClick={() => setShowModal(false)}>No</button>
+          </div>
+        </div>
+      )}
+
       <style jsx>{`
         .connect-button {
           position: absolute;
@@ -82,6 +110,33 @@ const Connect = () => {
           border-radius: 4px;
           cursor: pointer;
           text-decoration: none;
+        }
+
+        .modal {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background-color: rgba(0, 0, 0, 0.5);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .modal-content {
+          background-color: #EA7C69;
+          padding: 20px;
+          border-radius: 4px;
+          text-align: center;
+        }
+
+        .logout-button {
+          width: 100px; 
+          height: 40px; 
+          margin: 5px; 
+          background-color: #1F1D2B;
+          border-radius: 10px;
         }
       `}
       </style>
